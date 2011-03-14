d5642a3cfc9ee36ade866ed1f6beae4f952ace32
Daniel Robertson <Daniel Robertson daniel.robertson@escapemg.com>
2011-03-13 21:33:52 -0400

    first commit

---
+++ src/com/android/tippytipper/Tippytipper.java
@@ -0,0 +1,13 @@
+package com.android.tippytipper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tippytipper extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
